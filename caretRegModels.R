d9b4d49d7b75627eb8cc70653c862dba2dbd24b5
Abhey <abhey.pkl@gmail.com>
2019-05-31 11:51:40 +0530

    Added comments, changed metric lil bit

--- caretRegModels.R
+++ caretRegModels.R
@@ -5,7 +5,7 @@ library(heplots)
 dist_to_weight <- function(distance)
 {
   weight <- 1/distance
-  #weight <- weight/sum(weight)
+  weight <- weight/sum(weight)
   return(weight)
 }
 
@@ -13,7 +13,13 @@ dist_to_weight <- function(distance)
 #                                            cov = var(model_ready_data[,8])))
 # Not giving good results
 
-weight_train <- dist_to_weight(Moutlier(model_ready_data[,c(1:5,8)], plot = FALSE)$rd)
+#Gives us the number of covariates
+train_dim <- dim(model_ready_data)[2] - 2
+
+#Robust Mahalanobis Distance for weighting
+#Doing this over 1:(train_dim - 1) because can ignore promo_ind for weighting
+weight_train <- dist_to_weight(Moutlier(model_ready_data[,c((1:(train_dim-1)),dim(model_ready_data)[2])]
+                                        ,plot = FALSE)$rd)
 
 appl_weight <- function(x)
 {
@@ -23,17 +29,16 @@ appl_weight <- function(x)
 my_metric <- function(data, lev = NULL, model = NULL) 
 {
   diff <- data$pred - data$obs
-  met <- sum(((diff^2) * sapply(diff,appl_weight)))
+  met <- sum(((abs(diff)/data$obs) * sapply(diff,appl_weight)))
   c(MYM = met)
 }
 
-model_ready <- model_ready_data
-train_dim <- dim(model_ready)[2] - 2
-newm <- train(x = model_ready[,1:train_dim], y = model_ready$gap, method = "bagEarth", weights = weight_train,
-              metric = "MYM", maximize = FALSE, trControl = trainControl(summaryFunction = my_metric))
-exp <- predict(newm, newdata = model_ready[,1:train_dim])
+newm <- train(x = model_ready_data[,1:train_dim], y = model_ready_data$gap, method = "bagEarth", 
+              weights = weight_train, metric = "MYM", maximize = FALSE, 
+              trControl = trainControl(summaryFunction = my_metric))
+exp <- predict(newm, newdata = model_ready_data[,1:train_dim])
 exp <- ceiling(exp)
-View(cbind(exp, model_ready$gap))
+View(cbind(exp, model_ready_data$gap))
 
 newm2 <- train(x = model_ready_data[,1:train_dim], y = model_ready_data$gap, method = "earth",
                metric = "RMSE", maximize = FALSE)
